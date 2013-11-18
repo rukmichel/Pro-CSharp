8fdbde0cf91eed7a84b16511082e91d5953e650f
Rukamirwa Michel <rukmichel@yahoo.fr>
2013-11-18 22:59:47 +0100

    Properties of all variables on Street.cs

--- Traffic_Simulator/Street.cs
+++ Traffic_Simulator/Street.cs
@@ -18,27 +18,74 @@ namespace Traffic_Simulator
         /// </summary>
         private Direction _position;
 
+        public Direction Position
+        {
+            get { return _position ; }
+            set { _position = value; }
+        }
+
         /// <summary>
         /// Represents the first entrance lanes of the crossing.
         /// </summary>
         private Car[] _laneEnter1;
+
+        public Car[] LaneEnter1
+        {
+            get { return _laneEnter1; }
+
+            set 
+            { 
+                 for(int i = 0; i < value.Length; i++ )
+                 {
+                   _laneEnter1[i] = value[i]; 
+                 }
+            }
+        }
 
         /// <summary>
         /// Represents the second entrance lane of the crossing.
         /// </summary>
         private Car[] _laneEnter2;
 
+        public Car[] LaneEnter2
+        {
+            get { return _laneEnter2; }
+
+            set
+            {
+                for (int i = 0; i < value.Length; i++)
+                {
+                    _laneEnter2[i] = value[i];
+                }
+            }
+        }
+
         /// <summary>
         /// Represents the exit lane of the crossing.
         /// </summary>
         private Car[] _laneExit;
+
+        public Car[] LaneExit
+        {
+            get { return _laneExit; }
+
+            set
+            {
+                for (int i = 0; i < value.Length; i++)
+                {
+                    _laneExit[i] = value[i];
+                }
+            }
+        }
 
         /// <summary>
         /// Assigns atributes.
         /// </summary>
         /// <param name="t">The of crossing it belongs to.</param>
         /// <param name="d">Position of the street in the crossing.</param>
-        public Street(Type t, Direction d) {
+        public Street(Type t, Direction d)
+        {
+
         }
     }
 }

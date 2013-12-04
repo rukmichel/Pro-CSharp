07b2111a90d770b97bb85630211a6d047161eecb
Ashish Kumar <ashish@ashish.co>
2013-12-04 17:15:29 +0100

    Car: temp commit…

--- Traffic_Simulator/Car.cs
+++ Traffic_Simulator/Car.cs
@@ -100,9 +100,9 @@ namespace Traffic_Simulator
                 // check if car is exiting crossing
                 if (this.StreetIndex == 0)
                 {
-                    // check which crossing comes next
-                    String crossingLocation = this.Crossing.ID;
+                    String newCrossingID = "";
 
+                    // check if Car is exiting grid. If not, get next crossing's ID
                     switch (this.Direction)
                     {
                         case Direction.North:
@@ -112,6 +112,8 @@ namespace Traffic_Simulator
                                 this.exitGrid();
                                 return false;
                             }
+
+                            newCrossingID = Convert.ToString(this.Crossing.ID[0] - 1) + this.Crossing.ID[1];
 
                             break;
 
@@ -123,6 +125,8 @@ namespace Traffic_Simulator
                                 return false;
                             }
 
+                            newCrossingID = this.Crossing.ID[0] + Convert.ToString(this.Crossing.ID[1] + 1);
+
                             break;
 
                         case Direction.South:
@@ -132,6 +136,8 @@ namespace Traffic_Simulator
                                 this.exitGrid();
                                 return false;
                             }
+
+                            newCrossingID = Convert.ToString(this.Crossing.ID[0] + 1) + this.Crossing.ID[1];
 
                             break;
 
@@ -143,10 +149,14 @@ namespace Traffic_Simulator
                                 return false;
                             }
 
+                            newCrossingID = this.Crossing.ID[0] + Convert.ToString(this.Crossing.ID[1] - 1);
+
                             break;
                     }
 
-                    // if possible, move car onto street on next crossing
+                    // set new Crossing
+                    this.setCrossing(newCrossingID);
+                    goto EnterLane;
                 }
                 // check if the next position is vacant
                 else if (Street.LaneExit[StreetIndex - 1] == null)
@@ -160,8 +170,15 @@ namespace Traffic_Simulator
                 return false;
             }
             
+            EnterLane:
+
+            // if car is at the intersection
+            if (Street.Position == Direction.Center)
+            {
+
+            }
             // check if car is on laneEnter2
-            if ((Street.Position == Direction.North && this.Direction == Direction.East ||
+            else if ((Street.Position == Direction.North && this.Direction == Direction.East ||
                 Street.Position == Direction.East  && this.Direction == Direction.South ||
                 Street.Position == Direction.South && this.Direction == Direction.West ||
                 Street.Position == Direction.West  && this.Direction == Direction.North) &&
@@ -169,7 +186,7 @@ namespace Traffic_Simulator
             {
                 if (this.StreetIndex == Street.LaneEnter2.Length - 1)
                 {
-                    // check signal, and move to center
+                    return moveCenter();
                 }
                 else if (Street.LaneEnter2[StreetIndex + 1] == null)
                 {
@@ -186,7 +203,7 @@ namespace Traffic_Simulator
             {
                 if (this.StreetIndex == Street.LaneEnter1.Length - 1)
                 {
-                    // check signal and move to center
+                    return moveCenter();
                 }
                 else if (Street.LaneEnter1[StreetIndex + 1] == null)
                 {
@@ -213,5 +230,64 @@ namespace Traffic_Simulator
             //this.Turn = null;
         }
 
+        /// <summary>
+        /// Use probablity to figure out which direction the car should take
+        /// </summary>
+        /// <returns>Direction the car will turn at intersection</returns>
+        private Direction calculateTurn()
+        {
+
+            return Direction.Center;
+        }
+
+
+        private void setCrossing(string crossingID)
+        {
+            this.Crossing = Grid.getCrossing(crossingID);
+
+            // now that we know the Crossing we're in, time to figure out the Street
+            if (this.Direction == Direction.North)
+                this.Street = this.Crossing.StreetS;
+            else if (this.Direction == Direction.East)
+                this.Street = this.Crossing.StreetW;
+            else if (this.Direction == Direction.South)
+                this.Street = this.Crossing.StreetN;
+            else if (this.Direction == Direction.West)
+                this.Street = this.Crossing.StreetE;
+
+            // calculate where the car is going to turn based on probability
+            this.Turn = this.calculateTurn();
+        }
+
+        private bool moveCenter()
+        {
+            // we know we're already about to move to the center
+            // therefore, there are two things blocking our path
+
+            // first, let's check if the traffic light is green
+            switch (this.Direction)
+            {
+                case Direction.North:
+
+
+                    break;
+
+                case Direction.East:
+
+                    break;
+
+                case Direction.South:
+
+                    break;
+
+                case Direction.West:
+
+                    break;
+            }
+
+            // then, lets check if the  next spot is empty
+
+            return false;
+        }
     }
 }
